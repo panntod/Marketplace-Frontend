60e22ad77979dbd786b7c2d2f47c08843c5f5351
Pandhu Arya <pandhu.munjalindra@gmail.com>
2024-03-02 09:34:28 +0700

    feat: added index route

---
+++ src/routes/Router.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import ProtectRoute from "./ProtectRoute";
+import { LandingPage, NotFoundPage, Dashboard } from "../pages";
+import { ViewMenu } from "../pages/menu-management";
+import { Register, Login } from "../pages/auth";
+
+const Router = () => {
+	return (
+		<Routes>
+			<Route path="/" element={ <LandingPage/> } />
+			<Route path="/auth/register" element={ <Register/> } />
+			<Route path="/auth/login" element={<Login />} />
+			<Route path="/dashboard" element={
+				<ProtectRoute>
+					<Dashboard />
+				</ProtectRoute>
+			} />
+			<Route path="/master-menu" element={
+				<ProtectRoute>
+					<ViewMenu />
+				</ProtectRoute>
+			} />
+			
+			<Route path="/*" element={ <NotFoundPage/> } />
+		</Routes>
+	)
+};
+
+export default Router;
